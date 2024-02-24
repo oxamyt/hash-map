a39f97799a7723afadef789aafa5bf07e14f5c08
Oxamyt <dentez.danil@gmail.com>
2024-02-24 15:58:12 +0200

    add-resize-buckets

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+// Node class
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+}
+
+// Hashmap class with methods
 class Hashmap {
   constructor(key) {
     this.key = key;
-    this.buckets = new Array(16);
+    this.buckets = new Array(16).fill(null).map(() => []);
     this.loadFactor = 0.75;
     this.capacity = this.buckets.length;
     this.occupied = 0;
   }
 
+  // Hashing method
   hash(key) {
     let hashCode = 0;
 
@@ -17,144 +27,152 @@ class Hashmap {
     return hashCode;
   }
 
+  // Modulo method
   modulo(hash) {
     return hash % 16;
   }
 
+  // Resize method to increase bucket array capacity
   resize() {
-    const oldArray = this.buckets;
+    const oldBuckets = this.buckets;
     this.capacity *= 2;
-    this.buckets = new Array(this.capacity);
+    this.buckets = new Array(this.buckets.length * 2).fill(null).map(() => []);
     this.occupied = 0;
 
-    for (let oldBucket of oldArray) {
-      if (oldBucket) {
-        for (let node of oldBucket) {
-          const newBucketIndex = this.modulo(this.hash(node.key));
-          if (!this.buckets[newBucketIndex]) {
-            this.buckets[newBucketIndex] = [node];
-            this.occupied++;
-          } else {
-            this.buckets[newBucketIndex].push(node);
-            this.occupied++;
-          }
-        }
+    for (let oldBucket of oldBuckets) {
+      for (let node of oldBucket) {
+        this.set(node.key, node.value);
       }
     }
   }
 
+  // Adding node to hashmap method
   set(key, value) {
-    const hashNode = new Node(key, value);
     const bucketIndex = this.modulo(this.hash(key));
-    if (bucketIndex < 0 || bucketIndex >= this.buckets.length) {
-      throw new Error("Trying to access index out of bound");
-    } else if (!this.buckets[bucketIndex]) {
-      this.buckets[bucketIndex] = [hashNode];
-      this.occupied++;
-    } else if (this.buckets[bucketIndex]) {
-      this.buckets[bucketIndex].push(hashNode);
-      this.occupied++;
+
+    const currentBucket = this.buckets[bucketIndex];
+    for (let node of currentBucket) {
+      if (node.key === key) {
+        node.value = value;
+        return;
+      }
     }
 
-    const currentLoadFactor = this.occupied / this.capacity;
+    const hashNode = new Node(key, value);
+    currentBucket.push(hashNode);
+    this.occupied++;
+
+    const currentLoadFactor = this.occupied / this.buckets.length;
     if (currentLoadFactor > this.loadFactor) {
       this.resize();
     }
   }
 
+  // Get node using key method
   get(key) {
     const bucketIndex = this.modulo(this.hash(key));
-    if (this.buckets[bucketIndex]) {
-      const nodeValue = this.buckets[bucketIndex][0].value;
-      console.log(`Your value of key ${key} is ${nodeValue}`);
+    const currentBucket = this.buckets[bucketIndex];
+    if (currentBucket) {
+      for (let node of currentBucket) {
+        if (node.key === key) {
+          console.log(`Your value of key ${key} is ${node.value}`);
+          return node.value;
+        }
+      }
+      console.log(`Key ${key} not found`);
+      return;
     }
   }
 
+  // Check if hashmap has key method
   has(key) {
     const bucketIndex = this.modulo(this.hash(key));
-    if (this.buckets[bucketIndex]) {
-      console.log(`HashMap has ${key} - true`);
-      return true;
-    } else {
+    const currentBucket = this.buckets[bucketIndex];
+    if (currentBucket) {
+      for (let node of currentBucket) {
+        if (node.key === key) {
+          console.log(`HashMap has ${key} - true`);
+          return true;
+        }
+      }
       console.log(`HashMap do not have ${key} - false`);
       return false;
     }
   }
 
+  // Remove node using key method
   remove(key) {
     const bucketIndex = this.modulo(this.hash(key));
-    if (this.buckets[bucketIndex]) {
-      this.buckets[bucketIndex].splice(0, 1);
-      console.log(`Success remove of key:${key} - true`);
-      return true;
-    } else {
+    const currentBucket = this.buckets[bucketIndex];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i].key === key) {
+          currentBucket.splice(i, 1);
+          console.log(`Success remove of key:${key} - true`);
+          this.occupied--;
+          return true;
+        }
+      }
       console.log(`Do not found key:${key} to remove - false`);
       return false;
     }
   }
 
+  // Total keys number method
   length() {
     let numberOfKeys = 0;
     for (let bucket of this.buckets) {
-      if (bucket !== undefined && bucket.length !== 0) {
-        numberOfKeys++;
+      for (let node of bucket) {
+        if (node.key) {
+          numberOfKeys++;
+        }
       }
     }
-    console.log(numberOfKeys);
+    console.log(`Total keys: ${numberOfKeys}`);
+    return numberOfKeys;
   }
 
+  // Clear buckets method
   clear() {
-    for (let bucket of this.buckets) {
-      if (bucket !== undefined && bucket.length !== 0) {
-        bucket.splice(0, 1);
-      }
-    }
+    this.buckets = new Array(this.buckets.length).fill(null).map(() => []);
+    this.occupied = 0;
   }
 
+  // Return an array of keys in hashmap method
   keys() {
     const arrayOfKeys = [];
     for (let bucket of this.buckets) {
-      if (bucket !== undefined && bucket.length !== 0) {
-        arrayOfKeys.push(bucket[0].key);
+      for (let node of bucket) {
+        arrayOfKeys.push(node.key);
       }
     }
     console.log(`Your array of keys:${arrayOfKeys}`);
+    return arrayOfKeys;
   }
 
+  // Return an array of values in hashmap method
   values() {
     const arrayOfValues = [];
     for (let bucket of this.buckets) {
-      if (bucket !== undefined && bucket.length !== 0) {
-        arrayOfValues.push(bucket[0].value);
+      for (let node of bucket) {
+        arrayOfValues.push(node.value);
       }
     }
     console.log(`Your array of values:${arrayOfValues}`);
+    return arrayOfValues;
   }
 
+  // Return an array of keys and values in hashmap method
   entries() {
     const arrayOfEntries = [];
     for (let bucket of this.buckets) {
-      if (bucket !== undefined && bucket.length !== 0) {
-        arrayOfEntries.push([bucket[0].key, bucket[0].value]);
+      for (let node of bucket) {
+        arrayOfEntries.push([node.key, node.value]);
       }
     }
     console.log(arrayOfEntries);
-  }
-}
-
-class Node {
-  constructor(key, value) {
-    this.key = key;
-    this.value = value;
+    return arrayOfEntries;
   }
 }
 
 const hashMap = new Hashmap();
-
-hashMap.get("Soda");
-hashMap.keys();
-hashMap.values();
-hashMap.entries();
-
-hashMap.length();
-console.log(hashMap.buckets);
